7d5d640124c7723ccd76ed6e4de0b2b66f5c40d1
nekoi <nekoi275@gmail.com>
2019-05-20 16:45:44 +0300

    remove button changes

--- classes.js
+++ classes.js
@@ -1,5 +1,5 @@
 function Model() {
-    var tableName = "clothes";
+    var tableName = 'clothes';
     var data = JSON.parse(localStorage.getItem(tableName));
     data = (data == null) ? {} : data;
     var idCounter = getLatestId();
@@ -19,7 +19,7 @@ function Model() {
         return id;
     };
 
-    this.readAll = function (id) {
+    this.readAll = function () {
         return data;
     };
 
@@ -31,32 +31,32 @@ function Model() {
 }
 
 function View() {
-    this.form = document.getElementById("clothes");
-    var table = document.getElementById("clothes-table");
+    this.form = document.getElementById('clothes');
+    var table = document.getElementById('clothes-table');
+    
+    var self = this;
 
     this.clearForm = function() {
-        var editableInputs = this.form.getElementsByClassName("editable");
-        [].forEach.call(editableInputs, function(eInput) {eInput.value = null});
+        $('.editable').val('');
+        $('.editable').removeClass('is-invalid is-valid');
     };
     
     this.addRow = function(rowData, id, removeFunction) {
-        var tr = document.createElement("tr");
-        for (const colName of ['type', 'brand', 'color', 'description', 'price', 'date', 'season']) {
-            var td = document.createElement("td");
+        var tr = document.createElement('tr');
+        for (var colName of ['type', 'brand', 'color', 'description', 'price', 'date', 'season']) {
+            var td = document.createElement('td');
             td.innerText = rowData[colName];
             tr.appendChild(td);
         };
-        var removeButton = document.createElement("button");
-        removeButton.setAttribute("data-id", id);
-        removeButton.innerText = "X";
-        removeButton.classList.add("btn");
-        removeButton.classList.add("btn-dark");
-        removeButton.classList.add("btn-sm");
-        removeButton.onclick = removeFunction;
-        td = document.createElement("td");
+        var removeButton = document.createElement('button');
+        $(removeButton).attr('data-id', id);
+        $(removeButton).addClass('btn btn-dark btn-sm');
+        $(removeButton).html('<span>&times;</span>');
+        $(removeButton).click(removeFunction);
+        td = document.createElement('td');
         td.appendChild(removeButton);
         tr.appendChild(td);
-        tr.setAttribute("data-id", id);
+        tr.setAttribute('data-id', id);
         table.tBodies[0].appendChild(tr);
     };
 
@@ -68,14 +68,15 @@ function View() {
 
 function Controller() {
     var model = new Model();
-    var view = new View();
-    this.view = view;
+    this.view = new View();
+    
+    var self = this;
 
     function getFormData(form) {
         var obj = {};
         for(i = 0; i < form.elements.length; i ++) {
             var paramName = form.elements[i].name;
-            if (paramName !== "") {
+            if (paramName !== '') {
                 obj[paramName] = form.elements[i].value;
             };
         };
@@ -85,11 +86,11 @@ function Controller() {
     function validate(form) {
         [].forEach.call(form.elements, function(elem) {
             if (elem.validity.valid) {
-                elem.classList.remove("is-invalid");
-                elem.classList.add("is-valid");
+                elem.classList.remove('is-invalid');
+                elem.classList.add('is-valid');
             } else {
-                elem.classList.add("is-invalid");
-                elem.classList.remove("is-valid");
+                elem.classList.add('is-invalid');
+                elem.classList.remove('is-valid');
             }
         });
         return [].every.call(form.elements, function(elem) {return !elem.validity.valueMissing});
@@ -97,24 +98,24 @@ function Controller() {
 
     function removeRow(event) {
         var target = event.target;
-        var id = target.getAttribute("data-id");
+        var id = target.getAttribute('data-id');
         model.delete(id);
-        view.removeRow(id);
+        self.view.removeRow(id);
     };
 
     this.loadData = function() {
         var data = model.readAll();
         var ids = Object.keys(data);
-        ids.forEach(function (id) {view.addRow(data[id], id, removeRow)});
+        ids.forEach(function (id) {self.view.addRow(data[id], id, removeRow)});
     };
 
     this.processForm = function() {
-        if (!validate(view.form)) {
+        if (!validate(self.view.form)) {
             return;
         };
-        var obj = getFormData(view.form);
+        var obj = getFormData(self.view.form);
         var id = model.create(obj);
-        view.addRow(obj, id, removeRow);
-        view.clearForm();
+        self.view.addRow(obj, id, removeRow);
+        self.view.clearForm();
     };
 }
