69f53b95d996f4b7b66af958711687073fded532
nekoi <nekoi275@gmail.com>
2019-05-28 18:14:19 +0300

    changed colorpicker into class

--- public/colorPicker.js
+++ public/colorPicker.js
@@ -1,57 +1,60 @@
-var colorPicker = {};
+function ColorPicker() {
 
-colorPicker.canvas  = $('canvas.color-palette');
-colorPicker.buildColorPalette = function() {
-    colorPicker.context = colorPicker.canvas[0].getContext('2d');
-    var gradient = colorPicker.context.createLinearGradient(0, 0, colorPicker.canvas.width() * 0.9, 0);
-    colorPicker.context.canvas.width = colorPicker.canvas.width()
-    colorPicker.context.canvas.height = colorPicker.canvas.height()
+    var canvas  = $('canvas.color-palette');
+    var context = canvas[0].getContext('2d');
+    var self = this;
 
-    gradient.addColorStop(0,    "rgb(255,   0,   0)");
-    gradient.addColorStop(0.15, "rgb(255,   0, 255)");
-    gradient.addColorStop(0.33, "rgb(0,     0, 255)");
-    gradient.addColorStop(0.49, "rgb(0,   255, 255)");
-    gradient.addColorStop(0.67, "rgb(0,   255,   0)");
-    gradient.addColorStop(0.84, "rgb(255, 255,   0)");
-    gradient.addColorStop(1, "rgb(255,   0,   0)");
+    this.buildColorPalette = function() {
+        var gradient = context.createLinearGradient(0, 0, canvas.width() * 0.9, 0);
+        context.canvas.width = canvas.width()
+        context.canvas.height = canvas.height()
 
-    colorPicker.context.fillStyle = gradient;
-    colorPicker.context.fillRect(0, 0, colorPicker.canvas.width() * 0.9, colorPicker.canvas.height());
+        gradient.addColorStop(0,    "rgb(255,   0,   0)");
+        gradient.addColorStop(0.15, "rgb(255,   0, 255)");
+        gradient.addColorStop(0.33, "rgb(0,     0, 255)");
+        gradient.addColorStop(0.49, "rgb(0,   255, 255)");
+        gradient.addColorStop(0.67, "rgb(0,   255,   0)");
+        gradient.addColorStop(0.84, "rgb(255, 255,   0)");
+        gradient.addColorStop(1, "rgb(255,   0,   0)");
 
-    gradient = colorPicker.context.createLinearGradient(0, 0, 0, colorPicker.canvas.height());
-    gradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
-    gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
-    gradient.addColorStop(0.5, "rgba(0,     0,   0, 0)");
-    gradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
+        context.fillStyle = gradient;
+        context.fillRect(0, 0, canvas.width() * 0.9, canvas.height());
 
-    colorPicker.context.fillStyle = gradient;
-    colorPicker.context.fillRect(0, 0, colorPicker.canvas.width(), colorPicker.canvas.height());
+        gradient = context.createLinearGradient(0, 0, 0, canvas.height());
+        gradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
+        gradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
+        gradient.addColorStop(0.5, "rgba(0,     0,   0, 0)");
+        gradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
 
-    gradient = colorPicker.context.createLinearGradient(0, 0, 0, colorPicker.canvas.height());
-    gradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
-    gradient.addColorStop(0.1,   "rgba(255, 255, 255, 1)");
-    gradient.addColorStop(0.9,   "rgba(0,     0,   0, 1)");
-    gradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
-    colorPicker.context.fillStyle = gradient;
-    colorPicker.context.fillRect(colorPicker.canvas.width() * 0.9, 0, colorPicker.canvas.width(), colorPicker.canvas.height());
-};
+        context.fillStyle = gradient;
+        context.fillRect(0, 0, canvas.width(), canvas.height());
 
-colorPicker.canvas.click(function(e) {
-    colorPicker.colorEventX = e.pageX - colorPicker.canvas.offset().left;
-    colorPicker.colorEventY = e.pageY - colorPicker.canvas.offset().top;
-    colorPicker.getColor();
-    colorPicker.drawSelector();
-});
+        gradient = context.createLinearGradient(0, 0, 0, canvas.height());
+        gradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
+        gradient.addColorStop(0.1,   "rgba(255, 255, 255, 1)");
+        gradient.addColorStop(0.9,   "rgba(0,     0,   0, 1)");
+        gradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
+        context.fillStyle = gradient;
+        context.fillRect(canvas.width() * 0.9, 0, canvas.width(), canvas.height());
+    };
 
-colorPicker.getColor = function(e) {
-    var imageData = colorPicker.context.getImageData(colorPicker.colorEventX, colorPicker.colorEventY, 1, 1);
-    colorsArray = imageData.data;
-    $('#color').val(colorsArray[0] + ',' + colorsArray[1] + ',' + colorsArray[2]);
-};
+    canvas.click(function(e) {
+        colorEventX = e.pageX - canvas.offset().left;
+        colorEventY = e.pageY - canvas.offset().top;
+        getColor();
+        drawSelector();
+    });
 
-colorPicker.drawSelector = function() {
-    colorPicker.buildColorPalette();
-    colorPicker.context.beginPath();
-    colorPicker.context.arc(colorPicker.colorEventX, colorPicker.colorEventY, 5, 0, 2 * Math.PI);
-    colorPicker.context.stroke();
+    function getColor(e)  {
+        var imageData = context.getImageData(colorEventX, colorEventY, 1, 1);
+        colorsArray = imageData.data;
+        $('#color').val(colorsArray[0] + ',' + colorsArray[1] + ',' + colorsArray[2]);
+    };
+
+    function drawSelector() {
+        self.buildColorPalette();
+        context.beginPath();
+        context.arc(colorEventX, colorEventY, 5, 0, 2 * Math.PI);
+        context.stroke();
+    };
 };
