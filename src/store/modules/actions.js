75c04e1a4b4fc631f9aa875f4ceb2851034c4cbe
Nekoi <nekoi275@gmail.com>
2020-01-28 21:09:58 +0200

    moved actions to separate module

---
+++ src/store/modules/actions.js
@@ -0,0 +1,45 @@
+import api from '../../api/api.js'
+
+const actions = {
+    loadData({ commit, rootState }) {
+        api.get(rootState.table.current, json => {
+            commit("setData", json);
+            commit("showData");
+        }, reason => console.error(reason));
+    },
+    add({ commit, rootState }) {
+        rootState.form.currentData.table = rootState.table.current;
+        api.add(rootState.form.currentData, json => commit("add", json), reason => console.error(reason));
+    },
+    edit({ commit, rootState }, table) {
+        if (table) {
+            rootState.form.currentData.table = table;
+        } else {
+            rootState.form.currentData.table = rootState.table.current;
+        }
+        var update = Object.assign({}, rootState.form.currentData);
+        delete update._id;
+        var data = {
+            query: {
+                '_id': rootState.form.currentData._id
+            },
+            update: update
+        }
+        api.edit(data, json => {
+            if (table) {
+                commit("move", json);
+            } else {
+                commit("edit", json);
+            }
+            commit("showData");
+        }, reason => console.error(reason));
+    },
+    delete({ commit, rootState }) {
+        var id = rootState.form.currentData._id;
+        api.delete(id, () => commit("remove", id), reason => console.error(reason));
+    }
+}
+
+export default {
+    actions
+}
--- src/store/store.js
+++ src/store/store.js
@@ -3,7 +3,7 @@ import Vuex from 'vuex'
 import sidebarModule from './modules/sidebar.js'
 import formModule from './modules/form.js'
 import tableModule from './modules/table.js'
-import api from '../api/api.js'
+import actionsModule from './modules/actions.js'
 
 Vue.use(Vuex);
 
@@ -11,7 +11,8 @@ export const store = new Vuex.Store({
     modules: {
         sidebar: sidebarModule,
         form: formModule,
-        table: tableModule
+        table: tableModule,
+        actions: actionsModule
     },
     mutations: {
         showData(state) {
@@ -38,44 +39,5 @@ export const store = new Vuex.Store({
             });
             state.table.view[state.table.current] = data;
         },
-    },
-    actions: {
-        loadData(ctx) {
-            api.get(ctx.state.table.current, json => {
-                ctx.commit("setData", json);
-                ctx.commit("showData");
-            }, reason => console.error(reason));
-        },
-        add(ctx) {
-            ctx.state.form.currentData.table = ctx.state.table.current;
-            api.add(ctx.state.form.currentData, json => ctx.commit("add", json), reason => console.error(reason));
-        },
-        edit(ctx, table) {
-            if (table) {
-                ctx.state.form.currentData.table = table;
-            } else {
-                ctx.state.form.currentData.table = ctx.state.table.current;
-            }
-            var update = Object.assign({}, ctx.state.form.currentData);
-            delete update._id;
-            var data = {
-                query: {
-                    '_id': ctx.state.form.currentData._id
-                },
-                update: update
-            }
-            api.edit(data, json => {
-                if (table) {
-                    ctx.commit("move", json);
-                } else {
-                    ctx.commit("edit", json);
-                }
-                ctx.commit("showData");
-            }, reason => console.error(reason));
-        },
-        delete(ctx) {
-            var id = ctx.state.form.currentData._id;
-            api.delete(id, () => ctx.commit("remove", id), reason => console.error(reason));
-        }
-    },
+    }
 });
